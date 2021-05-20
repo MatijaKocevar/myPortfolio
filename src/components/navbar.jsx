ed6b706d45acc3265f7e49bd25f6fe7ea431856d
MatijaKocevar <matija.kocev@gmail.com>
2021-05-20 14:54:56 +0200

    fixed pictures

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../logo.png";
+import logo from "../images/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-scroll";
